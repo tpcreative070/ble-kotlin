bbd66dbe921603c2b674366943e151a44630490c
Tran Thanh Phong <>
2025-09-05 15:03:45 +0700

    Upgrade to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "co.tpcreative.ble_koltin"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
