692105efb1bf4ace6b1af731dfe1d4f69c84f4a0
Tran Thanh Phong <>
2025-09-05 15:36:52 +0700

    changed repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
+        maven { setUrl("https://jitpack.io") }
         mavenCentral()
         gradlePluginPortal()
     }
